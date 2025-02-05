de6ce92b3305dd1fd8f0a743e644240a59fcff41
hf-chow <honfungchow@gmail.com>
2025-02-05 12:22:45 +0800

    add helper functions

--- btree.go
+++ btree.go
@@ -1,9 +1,14 @@
 package btree
 
+import "encoding/binary"
+
 const HEADER = 4
 const BTREE_PAGE_SIZE = 4096 // Typical OS page size allocate 4kb
 const BTREE_MAX_KEY_SIZE = 1000
 const BTREE_MAX_VAL_SIZE = 3000
+
+const BNODE_NODE = 1
+const BNODE_LEAF = 2
 
 type BNode []byte
 
@@ -14,10 +19,34 @@ type BTree struct {
 	del 	func(uint64)
 }
 
+// | type | nkeys | pointers |  offsets  | key-values | unused |
+// |  2B  |   2B  | nkeys*8B |  nkeys*2B |    ...	  |  	   |
+
+func (node BNode) btype() uint16 {
+	return binary.LittleEndian.Uint16(node[0:2])
+}
+
+func (node BNode) nkeys() uint16 {
+	return binary.LittleEndian.Uint16(node[2:4])
+}
+
+func (node BNode) setHeader(btype uint16, nkeys uint16) {
+	binary.LittleEndian.PutUint16(node[0:2], btype)
+	binary.LittleEndian.PutUint16(node[2:4], nkeys)
+}
+
+func (node BNode) getPtr(idx uint16) uint16 {
+	pos := HEADER + 8*idx
+	return binary.LittleEndian.Uint16(node[pos: ])
+}
+
+func (node BNode) setPtr(idx uint16, val uint64)
+
 func init() {
 	node1max := HEADER + 8 + 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE
 	if (node1max <= BTREE_PAGE_SIZE) {
 		return
 	}
 }
+
 

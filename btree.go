95497cd63dd73ff40b687ce6351a5277d8b14e25
hf-chow <honfungchow@gmail.com>
2025-02-05 12:05:39 +0800

    first commit

---
+++ btree.go
@@ -0,0 +1,23 @@
+package btree
+
+const HEADER = 4
+const BTREE_PAGE_SIZE = 4096 // Typical OS page size allocate 4kb
+const BTREE_MAX_KEY_SIZE = 1000
+const BTREE_MAX_VAL_SIZE = 3000
+
+type BNode []byte
+
+type BTree struct {
+	root 	uint64
+	get 	func(uint64) []byte
+	new		func([]byte) uint64
+	del 	func(uint64)
+}
+
+func init() {
+	node1max := HEADER + 8 + 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE
+	if (node1max <= BTREE_PAGE_SIZE) {
+		return
+	}
+}
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hf-chow/gosql
+
+go 1.22.6
